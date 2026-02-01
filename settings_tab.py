c8570f764ba84e16bf29e0a956c0bdfb3dd3d7bd
Keira <160556645+keihwi@users.noreply.github.com>
2026-01-31 22:46:32 -0500

    changed fonts and stuff

--- settings_tab.py
+++ settings_tab.py
@@ -5,7 +5,7 @@ def create_settings_tab(parent, logout_callback):
     label = ctk.CTkLabel(frame, text="Settings", font=("Cooper Black", 20))
     label.pack(pady=20)
 
-    logout_button = ctk.CTkButton(frame, text="Logout", command=logout_callback, font=("Cooper Black", 16))
+    logout_button = ctk.CTkButton(frame, text="logout", command=logout_callback, font=("Cooper Black", 16))
     logout_button.pack(pady=12)
     
     return frame
